feb926b5bf2cf73e21b0e9105c8efad380897751
Joaquin Navarro <joaquinutnfrt@gmail.com>
2023-07-18 13:35:04 -0300

    adding initial structure

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+const Home = () => {
+  const [menu, setMenu] = useState()
+
+  const getFood = () => {
+
+  }
+
+  useEffect(()  => {
+  }, [])
+  
+  
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Home
