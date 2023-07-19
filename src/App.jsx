fb7f7b8c09ffc265333346293fab36a9c915cfe9
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-07-19 17:17:16 -0300

    se sube resolucion de los ABM menu, usuario y pedidos, queda en produccion/resolver las operciones de la misma con el backend y las validaciones de los formularios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
-
+import TablaAdministracion from './components/TablaAdministracion/TablaAdministracion';
+import {Outlet, Link, Route , Routes } from 'react-router-dom';
+import { pedidosPrueba , usuariosPrueba, menuPrueba ,cabeceraTablaMenu, cabeceraTablaUsuario, cabeceraTablaPedido } from './helpers/helpDB';
 function App() {
-
 
   return (
     <>
-     <h1>Prueba de la rama dev</h1>
+     <nav> 
+      <Link  to={'/administracionMenu'}> Administración Menú</Link>
+      <Link  to={'/administracionUsuario'}> Administración Usuarios </Link>
+      <Link  to={'/administracionPedido'}>Administración Pedidos</Link>
+     </nav>
+     <Outlet />
+<Routes>
+  <Route  path='administracionMenu' element={ <TablaAdministracion cabecera={cabeceraTablaMenu} title={'Menú'} opcion='menu' informacion={menuPrueba}/>}/>
+  <Route  path='administracionUsuario' element={ <TablaAdministracion cabecera={cabeceraTablaUsuario} title={'Usuario'} opcion='usuario' informacion={usuariosPrueba}/>}/>
+  <Route  path='administracionPedido' element={ <TablaAdministracion cabecera={cabeceraTablaPedido} title={'Pedido'} opcion='pedido'  informacion={pedidosPrueba}/>}/>
+</Routes>
+    
+    
+    
     </>
   )
 }
---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,70 @@
+import { useForm } from "react-hook-form";
+import Input from "../Input/Input";
+import './Formulario.css'; 
+
+function Formulario({handleShow, idItem , isRemoving, isEditing , opcion ,addItem}){
+    const {register,handleSubmit, reset} = useForm();
+    const Agregar = (data)=>{
+        {
+            opcion == 'menu'?data.urlImagen = URL.createObjectURL(data.urlImagen[0]) : null
+        }
+        
+        addItem(data);
+        alert(isEditing ? `${opcion} Modificado` : `${opcion} Creado`);
+        reset();
+    }
+return(
+    <form onSubmit={handleSubmit(Agregar)}>
+        {
+            isRemoving ? 
+            <>
+            <div className="contendor-btn">
+            <button className='btnConfirmar' >Confirmar</button>
+             <button type="button" className='btnCancelar'onClick={handleShow } > Cancelar </button>
+            </div>
+            </>
+            : 
+            <>
+           { 
+           opcion == 'menu' ? 
+           <>
+           <Input texto="Imagen" register={...register("urlImagen")}/>
+           <Input texto="Nombre"  register={...register("nombre")}/>
+           <Input texto="Descripción"  register={...register("descripcion")}/>
+           <Input texto="Precio"  register={...register("precio")}/>
+           <Input texto="Categoria"  register={...register("categoria")}/>
+           {
+            isEditing !== true ? 
+            <>
+            <Input texto="Publicado"  register={...register("publicado")}/>
+           <Input texto="Combo"  register={...register("combo")}/>
+            </>
+            : null
+           }
+           </>
+           :  opcion == 'usuario' ?
+           <>
+           <Input texto="Nombre" readOnly={isEditing ? true : false}  register={...register("nombre")} />
+          <Input texto="Email" readOnly={isEditing ? true : false}  register={...register("email")} />
+          <Input texto="Estado"  register={...register("estado")}/>
+          <Input texto="Rol"  register={...register("rol")}/>
+         </>
+           : opcion == 'pedido'?
+           <>
+          <Input texto="Fecha" readOnly={isEditing ? true : false}   register={...register("fecha")}/>
+          <Input texto="Usuario" readOnly={isEditing ? true : false}   register={...register("usuario")}/>
+          <Input texto="Menu" readOnly={isEditing ? true : false}   register={...register("menu")}/>
+          <Input texto="Estado"  register={...register("estado")}/>
+           </>
+           : null
+           }
+            <div className="contendor-btn">
+            <button className='btnConfirmar' >Guardar Menú</button>
+             <button type="button" className='btnCancelar'onClick={handleShow } > Cancelar </button>
+            </div>
+            </>
+        }
+    </form>
+);
+}
+export default Formulario; 
