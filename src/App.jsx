b6307046be89b2e5c06fc8df5acdac7dd8e9a448
´´juanmaRaimondo´´ <juanmarraimondo@gmail.com>
2023-07-23 19:55:54 -0300

    Subo Navbar incompleto. Falta las rutas y funcion de los botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
+import { Fragment } from 'react';
 import './App.css'
+import "./components/navbar/navbar"
+import { Container, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
+//import { Route } from 'react-router-dom';
+
 
 function App() {
 
 
   return (
+    // function Mostrar (params) { faltaria ver si esto esta bien y agregar las rutas y css
+      //if (user==true) {
+       // document.getElementById('administracion','pedidos').style.display='flex'
+      //}
+      
+    
     <>
-     <h1>Prueba de la rama dev</h1>
+    
+      <Container fluid className="navb"  >
+        <Navbar fluid className='mg-5'
+        >
+          <h5>Ficky Sangucheria</h5>
+
+          <Button id='administracion' variant="light" >Menus</Button>
+          <Button id='pedidos' variant="light" >Pedidos</Button>
+        </Navbar>
+        
+        <Container className='drop'>
+          <NavDropdown 
+            id="nav-dropdown-dark-example"
+            title="Registro" 
+            
+            menuVariant="dark"
+            className='text-primary'
+           
+          >
+
+            <NavDropdown.Item href="#action/3.1">Registrarse</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Login
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Productos</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Nosotros
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Container>
+      </Container>
+
+
+
     </>
   )
 }
 
-export default App; 
+export default App;
+
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const Nav = () => {
+  return (
+    <>
+      <div className="navb" >
+        <Navbar bg='myRed' variant='dark'>
+          <Navbar.Brand>
+            Ficky Sangucheria
+          </Navbar.Brand>
+        </Navbar>
+      </div>
+
+
+    </>
+  )
+}
+export default Nav;
