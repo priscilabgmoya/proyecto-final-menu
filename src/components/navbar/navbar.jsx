004a151b74a0a64943d2899153a8ac6313d91c39
´´juanmaRaimondo´´ <juanmarraimondo@gmail.com>
2023-07-24 12:24:39 -0300

    Subo correcciones en el css y en la ubicacion del navbar.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,49 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import './navbar.css';
+import { Container, Navbar, NavDropdown, Button } from 'react-bootstrap';
 
-const Nav = () => {
+const NavBars = () => {
+   // function Mostrar (params) { faltaria ver si esto esta bien y agregar las rutas y css
+      //if (user==true) {
+       // document.getElementById('administracion','pedidos').style.display='flex'
+      //}
   return (
     <>
-      <div className="navb" >
-        <Navbar bg='myRed' variant='dark'>
-          <Navbar.Brand>
-            Ficky Sangucheria
-          </Navbar.Brand>
+     
+     <Container fluid className="navb"  >
+        <Navbar fluid className='mg-5'
+        >
+          <h5>Ficky Sangucheria</h5>
+
+          <Button id='administracion' variant="light" >Menus</Button>
+          <Button id='pedidos' variant="light" >Pedidos</Button>
         </Navbar>
-      </div>
+        
+        <Container className='drop'>
+          <NavDropdown 
+            id="nav-dropdown-dark-example"
+            title="Registro" 
+            
+            menuVariant="dark"
+            className='text-primary'
+           
+          >
+
+            <NavDropdown.Item href="#action/3.1">Registrarse</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Login
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Productos</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Nosotros
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Container>
+      </Container>
 
 
     </>
   )
 }
-export default Nav;
+export default NavBars;
