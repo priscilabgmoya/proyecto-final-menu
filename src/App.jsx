efba7ec851ce40a86a59fcad1d7e331f374e6b42
Emiliano Acosta <emilianoacosta18@outlook.com>
2023-07-25 16:35:30 -0300

    se sube pagina de login y register de friky sangucheria

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import Login from './components/login'
+import { useState } from 'react'
+import Register from './components/register'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
-
-  return (
+return (
     <>
-     <h1>Prueba de la rama dev</h1>
-    </>
+    {/* <Register></Register>
+    <Routes>
+      <Route path='Login' element = {<Login/>}/>
+      
+      
+      <Routes/> */}
+    
+    <Login/>
+      </>
   )
 }
 
