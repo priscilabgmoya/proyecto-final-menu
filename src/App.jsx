48c474d8093c85ed44c3097a35971b2d04a7528c
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-07-28 11:57:46 -0300

    se agrega modificaciones de estructuracion del trabajo final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { Fragment } from 'react';
-
-
 import NavBars from './components/navbar/navbar.jsx';
 import Anosotros from './components/nosotros/nosotros.jsx';
 
@@ -11,20 +8,13 @@ function App() {
 
 
   return (
-
-    // function Mostrar (params) { faltaria ver si esto esta bien y agregar las rutas y css
-    //if (user==true) {
-    // document.getElementById('administracion','pedidos').style.display='flex'
-    //}
-
+    
 
     <>
       <NavBars />
-      <body>
-        <main>
-       <Anosotros/>
-      </main>
-    </body >
+   
+     
+
     </>
   )
 }
