13ba627a8155b892a9757a3d7a1e8f7eddbdeade
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2023-07-30 21:33:39 -0300

    Revert "Home "

--- src/helpers/helpProducts.js
+++
@@ -1,7 +0,0 @@
-
-export const getMenuByCategory = menu => menu?.reduce((menuSorted, menuItem) => {
-  const categoria = menuItem.categoria;
-  !menuSorted[categoria] ? menuSorted[categoria] = [] : null
-  menuSorted[categoria].push(menuItem);
-  return menuSorted;
-}, {});
