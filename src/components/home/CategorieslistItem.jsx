5480693d91881e95a326b79f5359a92398198f6c
Emiliano Acosta <emilianoacosta18@outlook.com>
2023-08-16 01:04:01 -0300

    se modifica style categorias

--- src/components/home/CategorieslistItem.jsx
+++ src/components/home/CategorieslistItem.jsx
@@ -4,9 +4,9 @@ const CategorieslistItem = props => {
   const {index, category} = props
   const {_id, nombre} = category
   return (
-    <li className="nav-item" key={index}>
-      <a className="nav-link  link-categoria" aria-current="page" href={`#${_id}`}>{nombre}</a>
-    </li>
+      <li className="nav-item" key={index}>
+        <a className="nav-link  link-categoria" aria-current="page" href={`#${_id}`}>{nombre}</a>
+      </li>
   )
 }
 
