1f2a189b44205019a818c4366d2416991ba6181a
Martin Rey <martinnrey03@gmail.com>
2023-08-25 17:58:03 -0300

    creacion del carrousel y reintegracion de un modal de modificacion de usuario

---
+++ src/components/PerfilUsuario/PerfilUsuario.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import "./PerfilUsuario.css"
+
+function PerfilUsuario() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch static backdrop modal
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                className='modal-color'
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modificacion de usuario</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <div className='mt-3'>
+                        <label htmlFor="usuario">nombre</label> <br />
+                        <input className='input-usuario' type="text" id='usuario' />
+                    </div> 
+                    <div className='mt-3'> 
+                        <label htmlFor="correo">correo electronico</label> <br />
+                        <input className='input-usuario' type="email"  id='correo' />
+                    </div>
+                    <div className='mt-3'>
+                        <label htmlFor="contraseña">contraseña</label> <br />
+                        <input className='input-usuario' type="text" id='contraseña' />
+                    </div> 
+                    <div className='mt-3'>
+                        <label htmlFor="recuperar">cambiar contraseña</label> <br />
+                        <input className='input-usuario' type="text"  id="recuperar" />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary">guardar cambios</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default PerfilUsuario;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import CategoriesList from './CategoriesList'
 import Products from './Products'
+import Carousel from '../Carousel/Carousel'
 
 const Home = ({agregarPedido}) => {
 
@@ -9,6 +10,7 @@ const Home = ({agregarPedido}) => {
     
     <>
       <main className='container-fluid'>
+        <Carousel></Carousel>
         <div className='row'>
           <div className="col-12 col-lg-12">
             <Products  agregarPedido ={agregarPedido}/>
